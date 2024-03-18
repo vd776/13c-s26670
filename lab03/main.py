59cfe1e661a2bd7b448c1274a8614e0e950d4938
s26670 <s26670@pjwstk.edu.pl>
2024-03-18 13:07:53 +0100

    Task03

--- lab03/main.py
+++ lab03/main.py
@@ -5,3 +5,10 @@ squares = [x ** 2 for x in range(1, 11)]
 # Task2
 def generate_squares(start, end):
     return [x ** 2 for x in range(start, end + 1)]
+
+
+# Task3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end + 1)]
+
