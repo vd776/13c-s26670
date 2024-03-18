586bb5d479c3648bdd180095880a244d840a4525
s26670 <s26670@pjwstk.edu.pl>
2024-03-18 13:11:35 +0100

    Task04

--- lab03/main.py
+++ lab03/main.py
@@ -12,3 +12,12 @@ class SquareGenerator:
     def generate_squares(self, start, end):
         return [x**2 for x in range(start, end + 1)]
 
+
+# Task4
+import math
+
+square_generator = SquareGenerator()
+squares = square_generator.generate_squares(1, 10)
+square_roots = [math.sqrt(x) for x in squares]
+print(square_roots)
+
